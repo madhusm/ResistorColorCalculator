3c40ea078f2840514af8bee166a2f675f50f7111
Madhu Manjunath <madhu.manjunath@united.com>
2017-11-26 21:48:13 -0600

    All Tests Included

--- TestResistorCalculator.UnitTests/HomeControllerUnitTest.cs
+++ TestResistorCalculator.UnitTests/HomeControllerUnitTest.cs
@@ -38,5 +38,12 @@ namespace TestResistorCalculator.UnitTests
             sut.WithCallTo(p => p.Index(res)).ShouldRenderDefaultView();
 
         }
+        [TestMethod]
+        public void ShouldHaveViewBags()
+        {
+            var sut = new HomeController();
+            //sut.ViewBag.Message.ToString().Should().ShouldBeEquivalentTo("");
+
+        }
     }
 }

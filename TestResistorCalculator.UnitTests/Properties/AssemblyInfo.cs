b779e60f2f43cce296c822624b8bb1ef45463aa9
Madhu Manjunath <madhu.manjunath@united.com>
2017-11-26 19:32:52 -0600

    Unit Test

---
+++ TestResistorCalculator.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestResistorCalculator.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestResistorCalculator.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("22adebdc-c19a-47b5-9f32-c6e8c014ac92")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestResistorCalculator.UnitTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ResistorColorCalculator.Models;
+
+namespace TestResistorCalculator.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShouldReturnZeroWhenBandValueIsNull()
+        {
+            var sut = new Resistor();
+            var isReturningZero = sut.CalculateOhmValue();
+            Assert.AreEqual(isReturningZero, 0.0);
+        }
+        [TestMethod]
+        public void ShouldReturnZeroWhenBandValueIsNull()
+        {
+            var sut = new Resistor();
+            var isReturningZero = sut.CalculateOhmValue();
+            Assert.AreEqual(isReturningZero, 0.0);
+        }
+    }
+}
